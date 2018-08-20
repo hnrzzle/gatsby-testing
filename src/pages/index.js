3ac4a78257dd55092a7423612ab81d6c5eedeebe
hnrzzle <henrique.j.oliveira1@gmail.com>
2018-08-19 17:03:15 -0700

    add the rules for logiklan

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>LogikLAN</h1>
+    <p>Welcome to LogikLAN</p>
+    <p>Be sure to check out <Link to="/the-rules">The Rules</Link></p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
---
+++ src/pages/the-rules.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const theRules = () => (
+  <div>
+    <h1>The Rules:</h1>
+    <p>Welcome to The Rules</p>
+    <ol>
+      <li>No smoking. Please smoke 40 or more feet away from the house.</li>
+
+      <li>Alcohol is allowed widthin reason. NO marijuana.</li>
+
+      <li>No fighting, brawling, punching, scratching, kicking, judo flips, or any other form of physical conduct.</li>
+
+      <li>No refunds will be given period.</li>
+
+      <li>No CRT monitors over 21". LCDs can be up to 27”.</li>
+
+      <li>No DHCP servers allowed. This includes Internet Connection Sharing.</li>
+
+      <li>Each attendee must sign a waiver stating that they take responsibility for any damage or loss that may happen to their equipment or person as a result of participation in LogikLAN. This means that anything you take into the event or win at the event, you are liable for.</li>
+
+      <li>No hacking or intentional network disruption. You WILL be ejected.</li>
+
+      <li>Do not tamper with anyone else's equipment but your own.</li>
+
+      <li>You are expected to maintain good hygiene fresh. Complaints about your body odor will result in removal from the event until you remedy the situation. (If someone smells – you need to report it to the help desk – we don’t actively seek out people who smell and rely on your input to make changes / take action)</li>
+
+      <li>Clean up after yourself. Trash bags/cans will be on site.</li>
+
+      <li>If you damage or break any equipment belonging to, lent to, or donated to the LogikLAN organization, you will be held responsible to pay for the damages to or replacement of the item.</li>
+
+      <li>Distributing Pornography to a minor or possessing pornography as a minor is illegal and not allowed</li>
+
+      <li>Your total power consumption can not exceed 650W (Monitor and Tower)</li>
+
+      <li> Logik always has final say. Do not argue with Him. </li>
+    </ol>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
+
+export default theRules;
